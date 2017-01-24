77a9246fe0ff1552fffd1043308a11490e35b865
nettapper <connerdunn7399@gmail.com>
2017-01-24 08:59:03 -0700

    printing out the params from the QUERY_STRING

--- hello.py
+++ hello.py
@@ -3,11 +3,20 @@
 import os
 import json
 from pprint import pprint
+import urlparse
 
 # print "Content-Type: text/html"
-# print "Content-Type: text/plain"
-print "Content-Type: application/json"
+print "Content-Type: text/plain"
+# print "Content-Type: application/json"
 print  # Needed to seperate the headers from the body for the http stuff!
 
 # pprint(dict(os.environ))
-print(json.dumps(dict(os.environ)))
+
+eVars = dict(os.environ)
+params = urlparse.parse_qs(eVars['QUERY_STRING'])
+
+# print(json.dumps(eVars))
+# print("------------")
+# print(eVars['QUERY_STRING'])
+# print("------------")
+print(params)
