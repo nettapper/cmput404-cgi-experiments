a2d870bd9bb8fefd195269e16d08f9057954ead9
nettapper <connerdunn7399@gmail.com>
2017-01-24 08:44:57 -0700

    Using a different pretty printing method now

--- hello.py
+++ hello.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
 import os
+import json
 from pprint import pprint
 
 # print "Content-Type: text/html"
-print "Content-Type: text/plain"
+# print "Content-Type: text/plain"
+print "Content-Type: application/json"
 print  # Needed to seperate the headers from the body for the http stuff!
 
-pprint( dict(os.environ) )
+# pprint(dict(os.environ))
+print(json.dumps(dict(os.environ)))
