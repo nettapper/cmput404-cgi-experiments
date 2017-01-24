79099cecd0a4409ffded0fa7d93fb93e232df874
nettapper <connerdunn7399@gmail.com>
2017-01-24 09:05:36 -0700

    let's show the user agent environ vars too

--- hello.py
+++ hello.py
@@ -10,13 +10,15 @@ print "Content-Type: text/plain"
 # print "Content-Type: application/json"
 print  # Needed to seperate the headers from the body for the http stuff!
 
-# pprint(dict(os.environ))
-
 eVars = dict(os.environ)
 params = urlparse.parse_qs(eVars['QUERY_STRING'])
 
+# pprint(eVars)
+# print("------------")
 # print(json.dumps(eVars))
 # print("------------")
 # print(eVars['QUERY_STRING'])
 # print("------------")
 print(params)
+print("------------")
+print(eVars['HTTP_USER_AGENT'])
