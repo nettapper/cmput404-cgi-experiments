eeb4110dd26d9ed9123fe41175ff8534f792fe03
nettapper <connerdunn7399@gmail.com>
2017-01-24 08:34:54 -0700

    ouput the environment vars & compare py to curl outputs

--- hello.py
+++ hello.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 
+import os
+from pprint import pprint
+
 # print "Content-Type: text/html"
 print "Content-Type: text/plain"
 print  # Needed to seperate the headers from the body for the http stuff!
-print "hello world"
+
+pprint( dict(os.environ) )
