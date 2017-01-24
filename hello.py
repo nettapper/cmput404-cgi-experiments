c36a5289fab248dc0292a55d8cdc2ba1b9bb3269
nettapper <connerdunn7399@gmail.com>
2017-01-24 08:23:58 -0700

    We want to say hello world through our cgi / python script

---
+++ hello.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+# print "Content-Type: text/html"
+print "Content-Type: text/plain"
+print  # Needed to seperate the headers from the body for the http stuff!
+print "hello world"
